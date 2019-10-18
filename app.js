eb8eeec0a4a19ae5e54580b683a7d7998bd9de82
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2019-10-17 19:00:37 -0700

    Update app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+var CloudmersiveConvertApiClient = require('cloudmersive-convert-api-client');
+var defaultClient = CloudmersiveConvertApiClient.ApiClient.instance;
+
+// Configure API key authorization: Apikey
+var Apikey = defaultClient.authentications['Apikey'];
+Apikey.apiKey = 'YOUR API KEY';
+
+
+
+var apiInstance = new CloudmersiveConvertApiClient.ConvertWebApi();
+
+var inputRequest = new CloudmersiveConvertApiClient.HtmlToOfficeRequest(); // HtmlToOfficeRequest | 
+
+
+var callback = function(error, data, response) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('API called successfully. Returned data: ' + data);
+  }
+};
+apiInstance.convertWebHtmlToDocx(inputRequest, callback);
+
+
+
+
